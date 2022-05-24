7227029f4ab36eed6b0540db79906e45ac972b39
damijin <103578220+damijin@users.noreply.github.com>
2022-05-24 15:04:54 +0900

    Add files via upload
    push test2

---
+++ SeongHyun.java
@@ -0,0 +1,13 @@
+package com.kh.main;
+
+public class SeongHyun {
+
+	public static void main(String[] args) {
+		
+		System.out.println("seong hyun zzang");
+		
+		System.out.println("test2");
+
+	}
+
+}
