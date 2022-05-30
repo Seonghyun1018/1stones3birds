6f4fee87591a7e877598cf667c0c5499ec48350d
SEONGHYUN\Seonghyun <qkrtjdgus101@naver.com>
2022-05-30 21:01:16 +0900

    Revert "수정"
    This reverts commit d0d9255634d5966a44fe33a1b82207089704daa7.

--- libraryPrj/src/admin/Admin.java
+++ libraryPrj/src/admin/Admin.java
@@ -22,6 +22,7 @@ public class Admin{
 	//관리자 기능 
 	
 	//책 추가하기
+	//도서 정보 조회
 	//회원 삭제?..
 	//도서 상태 갱신?
 	
@@ -63,8 +64,7 @@ public class Admin{
 					loginAdmPwd = dbPwd;
 					System.out.println(" ****접속 완료**** ");
 					//isLogin = true;
-					
-					adminMenu();
+				
 					
 				}
 			}
@@ -79,16 +79,20 @@ public class Admin{
 		}
 	
 
-
+		adminMenu();
 		
 }
 	
+public void adminMenu() {	
 	
-	public void adminMenu() {	
+	
 		
 		boolean isTrue = true;
 		
+		
 		// 1. 관리자 로그인 하기 while + switch
+		
+		
 		
 		while(isTrue){
 			 System.out.println("--------------------------------------------------------");
@@ -97,7 +101,7 @@ public class Admin{
 		     System.out.println("원하는 번호를 입력해주세요. : \n");
 		     System.out.println(" 1 -  도서 추가");
 		     System.out.println(" 2 -  회원 삭제");
-//		     System.out.println(" 3 -  뒤로 기기"); //어드민한테만 액세스 권한 가짐 일반회원 X  
+		     System.out.println(" 3 -  뒤로 기기"); //어드민한테만 액세스 권한 가짐 일반회원 X  
 		     System.out.println("-----------------------------------------\n");
 		     
 		     int n = Util.scInt();
@@ -105,14 +109,14 @@ public class Admin{
 				//케이스 추가할 거 있으면 case 4,5,6..... 클래스명().메소명(); break; 추가
 				
 				switch(n) {
-				case 1 : bookInsert(); break;
-				case 2 : deleteMember(); break;
-//				case 3 : break; 
+				case 1 :  break;
+				case 2 :  break;
+				case 3 :  break; 
 				default : System.out.println("시스템종료");
-				
 				}
-				
-		}	     
+		     
+				bookInsert();
+}	     
 }	     
 
 
@@ -122,113 +126,133 @@ public class Admin{
     // 회원 삭제
 	public void deleteMember() {
 		
-		//삭제할 회원의 아이디 입력받기
-		System.out.println("삭제할 회원의 아이디를 입력해주세요.");
-		String id = Util.sc.nextLine();
+		boolean istrue = true;
 		
-		Connection conn = OracleDB.getOracleConnection();
-		
-		String sql = "SELECT MNO FROM MEMBER WHERE MID = " + id;
-		
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		PreparedStatement pstmt2 = null;
-		
-		try {
-			pstmt = conn.prepareStatement(sql);
-			rs = pstmt.executeQuery();
+		while(istrue) {
+			System.out.println("=====회원 삭제=====");
+			System.out.print("정말 삭제하시겠습니까?");
+			System.out.print("1. 예 ");
+			System.out.print("2. 아니오 ");
 			
-			if(rs.next()) {
-				
-				boolean istrue = true;
-				
-				while(istrue) {
-					System.out.println("=====회원 삭제=====");
-					System.out.print("정말 삭제하시겠습니까?");
-					System.out.print("1. 예 ");
-					System.out.print("2. 아니오 ");
-					
-					int n = Util.scInt();
-					
-					if(n == 1) {
-						
-						int mno = rs.getInt("MNO");
-						
-						String dsql = "DELETE FROM MEMBER WHERE MNO = " + mno;
-						
-						pstmt2 = conn.prepareStatement(sql);
-						int i = pstmt.executeUpdate();
-						
-						if(i == 1) {
-							System.out.println(id + " 회원이 삭제되었습니다.");
-						}
-				 		else {
-						System.out.println("회원 삭제를 취소하셨습니다.");
-				 		}	
-					}		
-				
-				}		
-			}
+			int n = Util.scInt();
 			
-				}catch (SQLException e) {
-				System.out.println(" SQL 예외 발생 ");
-				e.printStackTrace();
-				System.out.println(" ----접속 실패---- ");
-				}finally {
-				OracleDB.close(conn);
-				OracleDB.close(pstmt);
-				OracleDB.close(rs);
-				}
-		
-}		
-           	
+			switch(n) {
+			case 1 : new User().myInfo(); break;
+			case 2 : istrue = false; break;
+			default : System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+			
+			
+		}
+	}
+}           	
        
          //도서 상태 INSERT DELETE UPDATE
         // 1. 도서 추가
       	public void bookInsert() {
-      		
        		Connection conn = OracleDB.getOracleConnection();
-       		
-       		// 추가할 도서 입력
-       		System.out.println("추가할 도서의 제목을 입력하세요.");
-       		String bname = Util.sc.nextLine();
-       		
-       		System.out.println("도서의 장르 번호를 입력하세요.");
-       		int cno = Util.scInt();
-       		
-       		
         	// INSERT 
-//       		String sql = "INSERT INTO BOOK(BNO, CNO, BNAME) VALUES(999 ,"+ cno +"," + bname + ")";
-       		String sql = "INSERT INTO BOOK(BNO, CNO, BNAME) VALUES(BOOK_PK_SEQ.NEXTVAL,"+ cno +"," + bname + ")";
+       		String sql
+    		= "INSERT INTO BOOK(BNO, CNO, BNAME, BCOUNT,BUYDATE,RENT_COUNT) "
+    				     + "VALUES(BOOK_PK_SEQ.NEXTVAL, ?, ?, ?, BUYDATE(?, 'RRRR/MM/DD'), ?)";
+    		
     		PreparedStatement pstmt = null;
+    		ResultSet rs = null;
     		
     		try {
-    			pstmt= conn.prepareStatement(sql);
-    			int i = pstmt.executeUpdate();
+    			PreparedStatement pstmt2 = conn.prepareStatement(sql);
+    			pstmt2.setString(1, "BNO");
+    			pstmt2.setString(2, "BCOUNT");
+    			pstmt2.setString(3, "BUYDATE");
+    			pstmt2.setString(4, "RENT_COUNT");
+    			int result = pstmt2.executeUpdate();
     			
-    			//출력하기
-    			if(i == 1) {
-					System.out.print(cno);
-					System.out.print(" | ");
-					System.out.print(bname);
-					System.out.print(" | ");
-					System.out.println();
-					System.out.println("장르번호 : "+ cno + "책제목 : " +bname + "추가가 완료 되었습니다.");
-    			} else {
-    				System.out.println("도서 추가를 실패하셨습니다.");
-    			}
-				
+    			
     		} catch (SQLException e) {
     			e.printStackTrace();
     		} finally {
-    			OracleDB.close(pstmt);
-    			OracleDB.close(conn);
+    			close(pstmt);
+    			close(rs);
+    			close(conn);
     		}
     		
     		
     	}
 
-	}
 
+
+       	
+       	
+
+
+
+		private void close(Connection conn) {
+			
+		}
+
+
+
+		private void close(ResultSet rs) {
+			
+			
+		}
+
+
+
+		private void close(PreparedStatement pstmt) {
+			
+			
+		}
+
+
+
+		// 3. 도서 정보 삭제
+       	public void bookDelete() {
+       		Connection conn = OracleDB.getOracleConnection();
+        	// DELETE
+        	String sql = "INSERT ANO, APWD FROM ADMIN WHERE AID = ?";
+       		
+       		
+       		
+       		
+       	}
+
+     
+//       	
+//       	// 4. 도서 조회
+//       	public void bookInfo() {
+//       		Connection conn = getConnection();
+//       		
+//       		Book book = bd.bookSelectId(conn, bookId);
+//       		
+//       		close(conn);
+//       		
+//       		return book;
+//       	}
+//       	
+      
+    	
+  
+	
+	}
+	
+      
+	
+  
+//class
+
+ 
+ 
+
+
+           
+            
+
+            
+                            
+
+
+
+	
+	
 	
 	
